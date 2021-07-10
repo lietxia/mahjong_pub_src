f0dde4372f6dfcd4b45afdbbb0f569db7e56ce96
lietxia <lietxia@gmail.com>
2021-07-10 09:19:55 +0800

    1.0.34

--- mfzh/newindex.js
+++ mfzh/newindex.js
@@ -17,7 +17,7 @@ $.getJSON(window.hosturl + "api/data.php?t=cs_list&cid=1", function (data) {
         ["宁波線下日麻", 144751555, "https://jq.qq.com/?_wv=1027&k=5iHkeca"],
         ["苏州線下日麻", 260025364, "https://jq.qq.com/?_wv=1027&k=5FThhYl"],
         ["廈門線下日麻", 733157756, "https://jq.qq.com/?_wv=1027&k=5pPEyUP"],
-        ["深圳線下日麻", 461861930, "https://jq.qq.com/?_wv=1027&k=5vVh2x6"],
+        ["深圳竞技协会", 461861930, "https://jq.qq.com/?_wv=1027&k=5vVh2x6"],
         ["泸州線下日麻", 669578617, "https://jq.qq.com/?_wv=1027&k=5Kg09UD"],
         ["南京線下日麻", 872957333, "https://jq.qq.com/?_wv=1027&k=527uO4p"],
         ["昆明線下日麻", 370653876, "https://jq.qq.com/?_wv=1027&k=5DDlv1W"],
@@ -25,7 +25,8 @@ $.getJSON(window.hosturl + "api/data.php?t=cs_list&cid=1", function (data) {
         ["西北竞技麻雀联盟", 620904958, "https://jq.qq.com/?_wv=1027&k=B1fMUwLN"],
         ["济南立直麻将", 835228612, "https://jq.qq.com/?_wv=1027&k=mxZ8HhEc"],
         ["贵阳立直联盟", 1142202920, "https://jq.qq.com/?_wv=1027&k=ZOgWTdXi"],
-        ["金华日麻面麻群", 877011936, "https://jq.qq.com/?_wv=1027&k=rSqrPPul"]
+        ["金华日麻面麻群", 877011936, "https://jq.qq.com/?_wv=1027&k=rSqrPPul"],
+        ["广州日麻俱乐部", 231709484, "https://jq.qq.com/?_wv=1027&k=kesIu2Sk"]
     ];
     var end_count = 0, running_count = 0;
     for (var i = 0; i < data.length; i++) {
